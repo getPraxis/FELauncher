f679b74eba67edcc7b8fd8932f6233bc6b5754d5
Darryl Ingalls <darryl@getpraxis.com>
2021-08-18 13:44:54 -0400

    Updating code to support multiple types of Ids or values to the Flow

--- force-app/main/default/aura/FlowEventListener/FlowEventListenerController.js
+++ force-app/main/default/aura/FlowEventListener/FlowEventListenerController.js
@@ -5,14 +5,17 @@
         let flowVariableName = event.getParam('flowVariableName');
         let sObjectName = event.getParam('sObject');
         let sFields= event.getParam('fields');
+        let dialogWidth = event.getParam('dialogWidth');
+        
         component.set('v.refreshPage', event.getParam('forcePageRefresh'));
-
+        component.set('v.dialogWidth', dialogWidth);
         if ($A.util.isUndefinedOrNull(ids) || $A.util.isUndefinedOrNull(flowName) ||
             $A.util.isUndefinedOrNull(flowVariableName) || $A.util.isUndefinedOrNull(sObjectName) ||
             $A.util.isUndefinedOrNull(sFields)) {
             helper.showErrorToastMessage('Error', 'Invalid Parameters from button click.  Please contact the System Administrator.');
             return;
         }
+        
         component.set('v.isModalOpened', true);   
         helper.getValuesFromSF(component, event,helper);
     },
@@ -26,11 +29,13 @@
     
     closeModalOnFinish : function(component, event) {
         let status = event.getParam('status');
+       
         if (status === 'FINISHED') {
             component.set('v.isModalOpened', false);
             if (component.get('v.refreshPage') == true) {
                 $A.get('e.force:refreshView').fire();
-            } 
+            }
+          
         }
     },
 })
